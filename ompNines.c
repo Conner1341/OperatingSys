a39ec5f107b73c33a8973637d597c77a3eff7090
Conner <clwherriges@gmail.com>
2016-03-21 15:54:52 -0500

    set up a function for the parallel section and cleaned up the code. Still seems to be running slower than it should

--- ompNines.c
+++ ompNines.c
@@ -4,17 +4,16 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define NUM_THREADS 4
+#define NUM_THREADS 8
 #define LENGTH 200000000
 
 int arr[LENGTH];
 int count = 0;
+int nines(void *param);
 
 int main() {
     int i;
     double start_time, end_time;
-    omp_lock_t lock;
-    omp_init_lock(&lock);
 
     /* intialize random number generator */
     srand((unsigned)time(NULL));
@@ -23,25 +22,30 @@ int main() {
     for (i = 0; i < LENGTH; i++)
         arr[i] = rand() % 100;
 
-    /* set the number of threads */
-    omp_set_num_threads(16);
-
     /* start the clock */
     start_time = omp_get_wtime();
-
-    #pragma omp parallel for shared(arr) private(count)
-    for (i = 0; i < LENGTH; i++) {
-        if (arr[i] == 99)
-            omp_set_lock(&lock);
-            count++;
-            omp_unset_lock(&lock);
-    }
+    count = nines(arr);
     end_time = omp_get_wtime();
-    printf("\n");
 
+    printf("\n");
     printf("The openMP code indicates that there are %d 99s in the array. \n\n", count);
     printf("The openMP used %f seconds to complete the execution.\n\n", end_time - start_time);
 
-    omp_destroy_lock(&lock);
     return 0;
+}
+
+int nines(void *param) {
+    omp_set_num_threads(NUM_THREADS);
+    int tid = omp_get_thread_num();
+    int num_threads = omp_get_num_threads();
+    int begin= tid * (LENGTH / num_threads);
+    int end = (tid + 1) * (LENGTH / num_threads) - 1;
+    int i, cnt;
+
+    #pragma omp parallel for shared(arr) private(count)
+    for (i = begin; i < end; i++) {
+        if (arr[i] == 99)
+            cnt++;
+    }
+    return cnt;
 }
